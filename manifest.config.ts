62ed4020cbc650fee9a6ad0070def47d66ef050d
Mogi Añover <miggyanover@gmail.com>
2026-04-14 14:15:18 +0800

    Remove tabs permission

--- manifest.config.ts
+++ manifest.config.ts
@@ -3,11 +3,11 @@ import { defineManifest } from "@crxjs/vite-plugin";
 export default defineManifest({
   manifest_version: 3,
   name: "Bottle It Back - AI Water Tracker",
-  version: "1.0.0",
+  version: "1.0.1",
   key: "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAyEO0iBoBz5+6KlTACu6GshOBkvWQ+ZPsQiIXNgYLJJKhe+rxTgh39D+9EbJjjVjgd37PdJMqFNDVchf3gP5G6C45gXEi36Tb67bacNmj1tYKyAz5qFuZS+e50AWyNDg0zwzz4NOZ/YxR/CQ8x3SjVWzNE+QANbadW0yDnQ3UAQhj1dxg2OkV6KWJHzQ7Jn/NPpFNwOvEaf5HUVz3SLVMWizfh6ujO72gJCA7uYFkzOvs8I6tf6PwYdfc+/P2wJsuFUHjqJ22oxACUnasiLDM1Lyepf3y4XTRs3emIYTLpHBxy/R57OcRtaNfOHsNN7GKg3htg2Hd0Qezz/0N0ODgKwIDAQAB",
   description:
     "Tracks supported AI websites and estimates water consumption from prompts. In partnership with the Planet Water Foundation.",
-  permissions: ["storage", "tabs"],
+  permissions: ["storage"],
   host_permissions: [
     "https://chatgpt.com/*",
     "https://chat.openai.com/*",
